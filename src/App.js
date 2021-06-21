8baa4cfc8206e791399743476878e33e91f27746
Maxi Badaró <maxibadaro@gmail.com>
2021-06-21 16:58:05 -0300

    ahora todo en español

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>React application - esta es la app del pueblo</h1>
+        <h1>Aplicación React - esta es la app del pueblo</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
