c19ec61ae9adfd8d9b3383fb3eda705143965136
Maxi Badaró <maxibadaro@gmail.com>
2021-06-27 22:26:47 -0300

    integrando sonar y tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react lik', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Aplicación/i);
   expect(linkElement).toBeInTheDocument();
 });
