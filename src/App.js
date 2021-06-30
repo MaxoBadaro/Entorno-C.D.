7e633520a64bc95915a737746e1d809d743c538a
Maxi Badaró <maxibadaro@gmail.com>
2021-06-30 18:14:56 -0300

    ultimos cambios

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Aplicación React - probando con Enzox2</h1>
+        <h1>Esto es un texto de prueba - Deploy en GH Pages</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('no está bien sacar esta palabra', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Aplicación/i);
+  const linkElement = screen.getByText(/Esto es un texto de prueba/i);
   expect(linkElement).toBeInTheDocument();
 });
